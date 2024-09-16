cd46776224f112d38416dc1606c149ac4c2b2867
mhebd <mhemonhossain@gmail.com>
2024-09-16 21:57:24 +0600

    app initial commit

---
+++ android/app/src/main/kotlin/com/example/coindesk/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coindesk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  _HomePageState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
